76728683094ae7c13e3984ecb5cb182585a88457
Ren Chang <hchang238.dev@gmail.com>
2024-09-09 22:24:17 -0500

    updated and organized code

---
+++ Decision.js
@@ -0,0 +1,8 @@
+class Decision extends JsonForm {
+    constructor(endpoint=false, option="", barriers = "") {
+        super()
+        this.endpoint
+        this.option
+        this.barriers
+    }
+}
--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 class JsonForm {
     constructor() {
-        this.id = self.crypto.randomUUID()
+        this.formId = self.crypto.randomUUID()
+        this.formTitle = ""
+    }
+
+    static deleteFormById(formId) {
+        let selector = `#${formId}`
+        $(selector).remove()
     }
 
     getEditableAttr() {
@@ -8,49 +14,54 @@ class JsonForm {
         for (var name in this) {
             variables.push(name)
         }
-        return variables.filter((variable) => variable != "id")
+        return variables.filter((variable) => !variable.startsWith("form"))
     }
 
     createInputs(selector) {
-        $(selector).append(`<form id="${this.id}"></form>`)
-        this.getEditableAttr().forEach( (attribute) => {
-            let formId = `#${this.id}`
-            $(formId).append(`<label for="${attribute}"> ${attribute.toUpperCase()}: </label>`)
-            $(formId).append(`<input type="text" id="${this.id}-${attribute}" name="${this.id}-${attribute}" placeholder="${attribute}"><br><br>`)
-        })
+        $(selector).append(`<form id="${this.formId}"><h3>${this.formTitle}</h3></form>`);
+        this.getEditableAttr().forEach((attribute) => {
+            let formSelector = `#${this.formId}`;
+            $(formSelector).append(`<label for="${attribute}"> ${attribute.toUpperCase()}: </label>`);
+            switch (typeof (this[attribute])) {
+                case "string":
+                    $(formSelector).append(`<input type="text" id="${this.formId}-${attribute}" name="${this.formId}-${attribute}" placeholder="${attribute}"><br><br>`);
+                    break;
+                case "boolean":
+                    $(formSelector).append(`<input type="checkbox" id="${this.formId}-${attribute}" name="${this.formId}-${attribute}" placeholder="${attribute}"><br><br>`);
+                    break;
+                case "number":
+                    $(formSelector).append(`<input type="number" id="${this.formId}-${attribute}" name="${this.formId}-${attribute}" placeholder="${attribute}"><br><br>`);
+                    break;
+                default:
+                    break;
+            }
+        });
     }
 
     updateInputs() {
         var that = this
 
-        $(document).keypress( function() {
+        $(document).on( "keypress", function() {
             that.getEditableAttr().forEach( (attribute) => {
-                let selector = `#${that.id}-${attribute}`
+                let selector = `#${that.formId}-${attribute}`
                 that[attribute] = $(selector).val()
-    
-                console.log(that)
             }) 
+
+            console.log(that)
         })
     }
 
     deleteInputs() {
-        let selector = `#${this.id}`
+        let selector = `#${this.formId}`
         $(selector).remove()
     }
 
     toString() {
         return JSON.stringify(this);
     }
-    
-    
 }
 
-class ContentBlock extends JsonForm {
-    constructor(header, body, image, caption) {
-        super()
-        this.header = header
-        this.body = body
-        this.image = image
-        this.caption = caption
-    }
-}
+
+$(document).on("keydown", ":input:not(textarea)", function(event) {
+    return event.key != "Enter";
+})
