42c225d68c79102aff7f66f8be17bfca23128d1f
Ren Chang <hchang238.dev@gmail.com>
2024-09-11 20:41:42 -0500

    changed to jsonforms

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+export const Header: FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src="./logo.svg" className="App-logo" alt="logo" />
+      <h1 className="App-title">ARHAT Json Tool</h1>
+      <p className="App-intro">Made With Json Forms</p>
+    </header>
+  </div>
+);
